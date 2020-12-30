65fc4430e2e7f7e8f2330a1e2ab146129ba420c4
ceize123 <hahaeating1@gmail.com>
2020-12-30 17:46:50 +0800

    test tri

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderContent, BlogArticle } from './pagesContent.js';
 
 function Blog() {
+  const [dropDown, setDropDown] = useState(false);
+  const handleDropDown = () => setDropDown(!dropDown);
+
 
   return (
     <>
     <div id='blogPage'>
       <HeaderContent title='Blog' sub='部落格'/>
       <main>
-        <div className='blogBar'>
+        <div className='blogBar' style={{marginBottom: dropDown ? '152px' : null}}>
           <input type='radio' name='category' id='first'/>
-          <label htmlFor='first'>最新文章</label>
+          <label htmlFor='first' style={{display: dropDown ? 'block' : null}}>最新文章</label>
           <input type='radio' name='category' id='second'/>
-          <label htmlFor='second'>全部文章</label>
+          <label htmlFor='second' style={{display: dropDown ? 'block' : null}}>全部文章</label>
           <input type='radio' name='category' id='third'/>
           <label htmlFor='third'>珠寶知識</label>
           <input type='radio' name='category' id='forth'/>
           <label htmlFor='forth'>產業趨勢</label>
           <input type='radio' name='category' id='fifth'/>
           <label htmlFor='fifth'>經驗分享</label>
+          <div>
+            <span className={`inputTri ${dropDown ? 'reverse' : null}`} onClick={handleDropDown}></span>
+          </div>
         </div>
         <BlogArticle />
       </main>
